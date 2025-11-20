8bb6998842414bef1e43c5bb7703779967e3c5a0
Shreyas G Gowda <shreyasg2526@gmail.com>
2025-11-20 10:41:23 +0530

    Initial commit

--- app/api/api.js
+++ app/api/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 const BaseURL = 'https://tsa-nie-next.vercel.app/'
 
-export const getTrainings = () => axios.get(`${BaseURL}/api/trainings`);
+export const getTrainings = () => axios.get(`${BaseURL}api/trainings`);
 
--- app/trainings/index.jsx
+++ app/trainings/index.jsx
@@ -1,12 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
+import { getTrainings } from '../api/api'
 
-export default function index() {
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
+export default function Trainings() {
+  const [trainingsdata,setTrainingdata] = useState([])
+
+  const fetchTrainings = async () => {
+    try{
+      const response = await getTrainings()
+      const data = response.data
+      setTrainingdata(data)
+      console.log(trainingsdata)
+}  catch(error){
+  console.error(error)
+}
+finally{
+
+}
+}
+useEffect(() => {
+  fetchTrainings()
+}
+)
+
+
 }
 
 const styles = StyleSheet.create({})
