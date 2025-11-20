20a1fd2267e9a7b7f2712d6e7b6bcc22890d03f6
Mohanraj M <126415700+MaX-NeO@users.noreply.github.com>
2025-11-20 09:39:33 +0530

    route_config

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+export default function Home() {
+    return (
+        <>
+            <View>
+                <Text>Welcome to MTD</Text>
+            </View>
+            
+        </>
+    )
+}
+
+const styles = StyleSheet.create({})
---
+++ app/trainings/[id]/apply.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Apply() {
+  return (
+    <View>
+      <Text>Apply</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
